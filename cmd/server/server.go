32d1ba292a0bd5f918e18216144f9d42b12b3073
transparentt <purplesaito@yahoo.co.jp>
2023-08-06 19:39:22 +0900

    Add echo

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/transparentt/login-server/config"
+	"github.com/transparentt/login-server/pkg/rethinkdb/logic"
 	"github.com/transparentt/login-server/pkg/rethinkdb/migration"
 	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
 )
+
+type SignUp struct {
+	UserName string `json:"user_name"`
+	PassWord string `json:"password"`
+}
 
 func main() {
 	config := config.LoadConfig()
@@ -21,6 +30,55 @@ func main() {
 
 	migrate(session)
 
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", health)
+	e.POST("/users", func(c echo.Context) error {
+		return signUp(c, session)
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8000"))
+
+}
+
+// Handler
+func health(c echo.Context) error {
+	return c.String(http.StatusOK, "OK")
+}
+
+func signUp(c echo.Context, rSession *r.Session) error {
+	signUp := new(SignUp)
+	if err := c.Bind(signUp); err != nil {
+		return err
+	}
+
+	existing, err := logic.GetUserByUserName(rSession, signUp.UserName)
+	if err != nil {
+		return err
+	}
+
+	if existing.ID != "" {
+		return c.String(http.StatusNotAcceptable, "NG")
+	}
+
+	user, err := logic.NewUser(signUp.UserName, signUp.PassWord)
+	if err != nil {
+		return err
+	}
+
+	_, err = user.Create(rSession)
+	if err != nil {
+		return err
+	}
+
+	return c.String(http.StatusCreated, "OK")
 }
 
 func migrate(session *r.Session) {
--- go.mod
+++ go.mod
@@ -3,18 +3,27 @@ module github.com/transparentt/login-server
 go 1.19
 
 require (
+	github.com/labstack/echo/v4 v4.11.1
 	github.com/oklog/ulid/v2 v2.1.0
 	golang.org/x/crypto v0.12.0
 	gopkg.in/rethinkdb/rethinkdb-go.v6 v6.2.2
 )
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.3.4 // indirect
 	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/sirupsen/logrus v1.0.6 // indirect
-	golang.org/x/net v0.10.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/term v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	gopkg.in/cenkalti/backoff.v2 v2.2.1 // indirect
 )
